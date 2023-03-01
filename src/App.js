a49c3e07409d35c9d0471437dcf483d174c08c82
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-03-01 17:01:57 +0200

    build: reset main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FeatherIcon from "feather-icons-react/build/FeatherIcon";
+import { useDispatch, useSelector } from "react-redux";
+import ListTodos from "./components/ListTodos";
+import { add, clear, remove } from "./features/toDoList";
 
-function App() {
+export default function Todos() {
+  const toDoList = useSelector((state) => state.toDolist);
+  const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex justify-center items-center font-bold text-2xl">
+      <div className="w-[40%] text-center">
+        <h1 className="text-6xl">Todos</h1>
+        <form
+          className="flex items-center text-lg w-full  shadow-xl border p-3 rounded-full"
+          onSubmit={(e) => e.preventDefault()}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="text"
+            value={inputValue || ""}
+            onChange={(event) => setInputValue(event.target.value)}
+            placeholder="add Todos"
+            className="w-full outline-none"
+          />
+          <button
+            type="submit"
+            onClick={() => {
+              if (inputValue) {
+                dispatch(add({ title: inputValue }));
+                setInputValue("");
+              }
+            }}
+          >
+            <FeatherIcon
+              icon="plus-circle"
+              fill="blue"
+              stroke="white"
+              size={40}
+            />
+          </button>
+        </form>
+        {toDoList?.length === 0 && <p className="text-2xl">No Todos</p>}
+        {toDoList?.map((item, i) => {
+          return (
+            <ListTodos
+              key={item.id}
+              {...item}
+              deleteTodo={() => dispatch(remove({ id: item.id }))}
+            />
+          );
+        })}
+        {toDoList?.length !== 0 && (
+          <button
+            onClick={() => dispatch(clear())}
+            className="text-base p-1.5 mt-10 rounded-full bg-red-600"
+          >
+            <FeatherIcon icon="x" stroke="white" />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/ListTodos.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { edit } from "../features/toDoList";
+import FeatherIcon from "feather-icons-react";
+export default function ListTodos({ title, id, deleteTodo }) {
+  const [isCheck, setIsCheck] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editValue, setEditValue] = useState(title);
+  const dispatch = useDispatch();
+  function editTodo() {
+    dispatch(edit({ id: id, title: editValue }));
+    setIsEdit(!isEdit);
+  }
+  return (
+    <div className="flex justify-between items-center py-3 border-b">
+      {isEdit ? (
+        <div className="flex gap-10 items-center">
+          <input
+            type="text"
+            onChange={(e) => setEditValue(e.target.value)}
+            className="border border-black"
+            value={editValue}
+          />
+          <button className="bg-gray-300 p-2 rounded-full" onClick={editTodo}>
+            <FeatherIcon icon="save" fill="white" />
+          </button>
+        </div>
+      ) : (
+        <div className="flex gap-10 items-center">
+          <input type="checkbox" onClick={() => setIsCheck(!isCheck)} />
+          {isCheck ? <p className="line-through">{title}</p> : <p>{title}</p>}
+        </div>
+      )}
+
+      <div className="flex gap-2">
+        <button className="text-red-600" onClick={() => deleteTodo(id)}>
+          <FeatherIcon icon="trash-2" />
+        </button>
+        <button
+          className="text-green-600 cursor-pointer"
+          onClick={() => setIsEdit(!isEdit)}
+        >
+          {isEdit ? (
+            <FeatherIcon icon="save" onClick={editTodo} />
+          ) : (
+            <FeatherIcon icon="edit-2" />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+}
