bab994d12f367a3710e83c52fff5b934ddebd6ae
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-03-05 21:31:21 +0200

    chore: delete all files

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todosListReducer from "../features/todos";
-const store = configureStore({
-  reducer: {
-    todosList: todosListReducer,
-  },
-});
-
-export default store;
